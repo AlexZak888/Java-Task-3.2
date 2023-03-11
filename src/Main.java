b28929e28c40e1dede859af73b7f1ead6e9a201a
Aleksandr Zakharov <laew2012@mail.ru>
2023-03-11 16:32:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double bmi = service.calculate(1.87, 98);
+        int rezultBmi = (int) bmi;
+        System.out.println(rezultBmi);
+    }
+}
